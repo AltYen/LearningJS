cc37b997235e4396bd318ebdee46457f9ce5e564
Altan Yenigün <altanyenigun@gmail.com>
2022-04-26 19:39:33 +0200

    notes-app

--- notes-app/script.js
+++ notes-app/script.js
@@ -1,10 +1,18 @@
 const addBtn=document.getElementById("add");
 
+const notes = JSON.parse(localStorage.getItem('notes'));
+
+if(notes){
+    notes.forEach(note=>{
+        addNewNote(note);
+    })
+}
+
 addBtn.addEventListener("click",()=>{
     addNewNote();
 });
 
-function addNewNote(){
+function addNewNote(text=''){
 
     const note=document.createElement("div");
     note.classList.add('note');
@@ -16,9 +24,9 @@ function addNewNote(){
             <button class="edit"><i class="fas fa-edit"></i></button>
             <button class="delete"><i class="fas fa-trash-alt"></i></button>
         </div>
-        <div class="main hidden">
+        <div class="main ${text ? '' : "hidden"}">
         </div>
-        <textarea></textarea>
+        <textarea class="${text ? "hidden" : ''}"></textarea>
     </div>`;
 
     const editBtn=note.querySelector('.edit');
@@ -28,6 +36,8 @@ function addNewNote(){
     const main=note.querySelector(".main");
     const textArea=note.querySelector("textarea");
 
+    textArea.value=text;
+    main.innerHTML=marked.parse(text);
 
     editBtn.addEventListener("click",()=>{
         main.classList.toggle('hidden');
@@ -36,19 +46,34 @@ function addNewNote(){
 
     deleteBtn.addEventListener("click",()=>{
         note.remove();
+
+        updateLS();
     });
 
     textArea.addEventListener("input",(e)=>{
         const {value} = e.target;
 
         main.innerHTML= marked.parse(value);
+
+        updateLS();
     });
 
     document.body.appendChild(note);
 
 }
 
+function updateLS(){
+     const notesText=document.querySelectorAll('textarea');
+      
+     const notes=[];
 
+     notesText.forEach(note => {
+         notes.push(note.value);
+     });
+
+     localStorage.setItem('notes',JSON.stringify(notes));
+
+}
 
 
 
